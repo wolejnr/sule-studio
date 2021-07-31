3f3be8f480c76e78d3d2ab1b792c9293ab8e4f22
Adewole Adewumi <wolejr@gmail.com>
2021-07-31 00:00:59 -0400

    Added local settings

--- sulestudios/settings.py
+++ sulestudios/settings.py
@@ -124,3 +124,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like there is no local file. You must be in production.")
